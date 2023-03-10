364e4ec4f56a740a8ff1f7dbabf138f7ab53eff2
SHAKEEB KHAN <shakeebsun@gmail.com>
2023-03-10 19:49:18 +0500

    pick image done

--- lib/screens/authentication/signup_screen.dart
+++ lib/screens/authentication/signup_screen.dart
@@ -1,10 +1,19 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import '../api/register_api.dart';
 import '../constants/colors.dart';
 import '../constants/customtextfield.dart';
 import 'login_screen.dart';
+import 'dart:io';
 
 class SignUpScreen extends StatefulWidget {
   static const routeName = '/signUpScreen';
@@ -41,6 +50,72 @@ class _SignUpScreenState extends State<SignUpScreen> {
   ApiScreen apiScreen = ApiScreen();
   bool isPassword = true;
   bool isConfirmPassword = true;
+  File? pickedImage;
+  String? imagePath;
+
+
+  // TODO Image Picker
+  Future getImage(ImageSource source) async {
+
+    PermissionStatus cameraStatus = await Permission.camera.request();
+    if (cameraStatus == PermissionStatus.granted) {
+      Fluttertoast.showToast(msg: "Permission Granted");
+      await ImagePicker().pickImage(source: source).then((value) {
+        if (value != null) {
+          imageCropperView(File(value.path));
+        }
+      });
+    }
+    else if (cameraStatus == PermissionStatus.denied) {
+      Fluttertoast.showToast(msg: "You need to give camera permission to pick image");
+    }
+    else if(cameraStatus == PermissionStatus.permanentlyDenied) {
+      Fluttertoast.showToast(msg: "Now you should go in app setting to give permission");
+      openAppSettings();
+    }return;
+
+  }
+
+  // TODO Image Cropper
+  imageCropperView(File path) async {
+    final croppedImage = await ImageCropper().cropImage(
+      sourcePath: path.path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      uiSettings: [
+        AndroidUiSettings(
+            toolbarTitle: 'Crop Image',
+            toolbarColor: Colors.green,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        IOSUiSettings(
+          title: 'Crop Image',
+        ),
+        WebUiSettings(
+          context: context,
+        ),
+      ],
+    );
+
+    if (croppedImage != null) {
+      imageCache.clear();
+      setState(() {
+        pickedImage = File(croppedImage.path);
+        imagePath = base64Encode(pickedImage!.readAsBytesSync());
+      });
+    } else {
+      log("not cropped image");
+      return '';
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +134,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   child: Text(
                     "Skabapp",
                     style: GoogleFonts.sofia(
-                        fontSize: 38.0,
+                        fontSize: 30.0,
                         color: firstColor.withOpacity(0.9),
                         fontWeight: FontWeight.w700),
                   ),
@@ -80,8 +155,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       ]),
                     )),
                 Container(
-                  height: 40,
-                  width: 270,
+                  height: 30,
+                  width: 230,
                   margin: const EdgeInsets.only(top: 10.0),
                   decoration: BoxDecoration(
                       color: facebookLogoColor,
@@ -99,6 +174,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       const Icon(
                         FontAwesomeIcons.facebook,
                         color: thirdColor,
+                        size: 20,
                       ),
                       Text(
                         "Log in with Facebook",
@@ -140,12 +216,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 const SizedBox(height: 10.0),
                 CustomTextField(
                   myController: emailController,
-                  validator: (String? value){
-                    if(value!.isEmpty){
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
                       return "Field is required";
-                    }else if(!regExpEmail.hasMatch(value)){
+                    } else if (!regExpEmail.hasMatch(value)) {
                       return "Please enter valid email";
-                    }else{
+                    } else {
                       return null;
                     }
                   },
@@ -153,36 +229,36 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 ),
                 const SizedBox(height: 10.0),
                 CustomTextField(
-                  myController: fNameController,
-                    validator: (String? value){
-                    if(value!.isEmpty){
-                      return "Field is required";
-                    }else if(value.length < 3){
-                      return "Characters should be at least 3";
-                    }else{
-                      return null;
-                    }
+                    myController: fNameController,
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return "Field is required";
+                      } else if (value.length < 3) {
+                        return "Characters should be at least 3";
+                      } else {
+                        return null;
+                      }
                     },
                     text: "Full Name"
                 ),
                 const SizedBox(height: 10.0),
                 CustomTextField(
-                  validator: (String? value){
-                    if(value!.isEmpty){
-                      return "Field is required";
-                    }else{
-                      return null;
-                    }
-                  },
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return "Field is required";
+                      } else {
+                        return null;
+                      }
+                    },
                     myController: usernameController,
                     text: "Username"
                 ),
                 const SizedBox(height: 10.0),
                 CustomTextFieldPassword(
-                    isObscure: isPassword,
-                    myController: passwordController,
-                     icon: IconButton(
-                    onPressed: (){
+                  isObscure: isPassword,
+                  myController: passwordController,
+                  icon: IconButton(
+                    onPressed: () {
                       setState(() {
                         isPassword = !isPassword;
                       });
@@ -194,22 +270,22 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       color: isPassword ? firstColor : Colors.blueGrey,
                     ),
                   ),
-                    validator: (String? value){
-                      if (value!.isEmpty) {
-                        return 'Please enter password';
-                      }else if(!regexPassword.hasMatch(value)){
-                        return "Enter valid password";
-                      }else{
-                        return null;
-                      }
-                    },
-                    text: "Password",
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter password';
+                    } else if (!regexPassword.hasMatch(value)) {
+                      return "Enter valid password";
+                    } else {
+                      return null;
+                    }
+                  },
+                  text: "Password",
                 ),
                 const SizedBox(height: 10.0),
                 CustomTextFieldPassword(
                     isObscure: isConfirmPassword,
                     icon: IconButton(
-                      onPressed: (){
+                      onPressed: () {
                         setState(() {
                           isConfirmPassword = !isConfirmPassword;
                         });
@@ -222,16 +298,82 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       ),
                     ),
                     myController: confirmPasswordController,
-                    validator: (String? value){
-                      if(value!.isEmpty){
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
                         return "Field is required";
-                      }else if(value != passwordController.text){
+                      } else if (value != passwordController.text) {
                         return "Both passwords don't match";
-                      }else{
+                      } else {
                         return null;
                       }
                     },
                     text: "Confirm Password"
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                       pickedImage != null ? const Text("Picked Done", style: TextStyle(
+                          color: darkJungleGreenColor,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17),) : const Text("Add Image", style: TextStyle(
+                           color: darkJungleGreenColor,
+                           fontWeight: FontWeight.w500,
+                           fontSize: 17),),
+                      InkWell(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (ctx) =>
+                                AlertDialog(
+                                  title: const Text("Select One",
+                                    style: TextStyle(fontSize: 20.0,
+                                        fontWeight: FontWeight.w600,
+                                        color: darkJungleGreenColor),),
+                                  actions: <Widget>[
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment
+                                          .spaceEvenly,
+                                      children: [
+                                        TextButton(
+                                            onPressed: () {
+                                              getImage(ImageSource.camera);
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text("Camera",
+                                              style: TextStyle(fontSize: 15.0,
+                                                  color: Colors.black87),)
+                                        ),
+                                        TextButton(
+                                            onPressed: () {
+                                              getImage(ImageSource.gallery);
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text("Gallery",
+                                              style: TextStyle(fontSize: 15.0,
+                                                  color: Colors.black87),)
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                          );
+                        },
+                        child: pickedImage != null ? CircleAvatar(
+                          radius: 30.0,
+                          backgroundColor: Colors.white,
+                          child: ClipOval(
+                              child: Image.file(File(pickedImage!.path), fit: BoxFit.cover,)),
+                        ) : CircleAvatar(
+                          radius: 30.0,
+                          backgroundColor: Colors.white,
+                          child: Image.asset("assets/images/image.png"),
+                        )
+                      )
+                    ],
+                  ),
                 ),
                 const SizedBox(
                   height: 9.0,
@@ -239,15 +381,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 MaterialButton(
                   onPressed: () {
                     print(_formKey.currentState!.validate());
-                    if(_formKey.currentState!.validate()) {
+                    if (_formKey.currentState!.validate()) {
                       print(_formKey.currentState!.validate());
                       print("Success");
+                      print("Image ${pickedImage!.path}");
                       apiScreen.register(
                         usernameController.text.toString(),
                         emailController.text.toString(),
                         fNameController.text.toString(),
                         passwordController.text.toString(),
                         confirmPasswordController.text.toString(),
+                        imagePath,
                         context,
                       );
                       usernameController.clear();
@@ -255,17 +399,22 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       fNameController.clear();
                       passwordController.clear();
                       confirmPasswordController.clear();
-                    }else{
+                    } else {
                       print("Failed to validate");
                     }
                   },
                   splashColor: secondColor,
                   hoverColor: firstColor,
-                  minWidth: 180,
+                  minWidth: 150,
+                  height: 30,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(9.0),
+                  ),
                   color: firstColor.withOpacity(0.8),
                   child: Text(
                     "Sign Up",
-                    style: GoogleFonts.oswald(color: secondColor, fontSize: 15.0),
+                    style: GoogleFonts.oswald(
+                        color: secondColor, fontSize: 13.0),
                   ),
                 ),
                 const SizedBox(
@@ -294,8 +443,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
                                   color: Colors.black87, fontSize: 15.0)),
                           const WidgetSpan(
                               child: SizedBox(
-                            width: 5.0,
-                          )),
+                                width: 5.0,
+                              )),
                           TextSpan(
                               text: "Log in",
                               style: GoogleFonts.roboto(
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -17,86 +17,88 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            color: firstColor.withOpacity(0.5),
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.topLeft,
-                  padding: const EdgeInsets.only(top: 100.0, left: 25.0),
-                  child: AnimatedTextKit(
-                    repeatForever: true,
-                    pause: const Duration(seconds: 20),
-                    animatedTexts: [
-                      TyperAnimatedText('Enjoy',
-                          textStyle: GoogleFonts.audiowide(
-                              color: Colors.black87,
-                              fontSize: 35.0,
-                              fontWeight: FontWeight.w800,
-                              letterSpacing: 1.0),
-                        speed: const Duration(milliseconds: 350),
-                        curve: Curves.bounceIn
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  padding: const EdgeInsets.only(top: 4.0, left: 25.0),
-                  // child: Text("cooking",
-                  //     style: GoogleFonts.audiowide(
-                  //         color: Colors.white,
-                  //         fontSize: 30.0,
-                  //         fontWeight: FontWeight.w400,
-                  //         letterSpacing: 1.0)),
-                  child: AnimatedTextKit(
-                    repeatForever: true,
-                    pause: const Duration(seconds: 13),
-                    animatedTexts: [
-                      TyperAnimatedText('cooking',
-                          textStyle: GoogleFonts.audiowide(
-                              color: thirdColor,
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 1.0),
-                          speed: const Duration(milliseconds: 370),
-                          curve: Curves.linear,
-                      )
-                    ],
-                  ),
-                ),
-                Container(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              color: firstColor.withOpacity(0.5),
+              child: Column(
+                children: [
+                  Container(
                     alignment: Alignment.topLeft,
-                    padding: const EdgeInsets.only(top: 20.0, left: 25.0),
-                    child: RichText(
-                      text: TextSpan(children: [
-                        TextSpan(
-                            text: "Delicious and detailed restaurant recipes",
-                            style: GoogleFonts.roboto(
-                                color: Colors.black87, fontSize: 13.0)),
-                        TextSpan(
-                            text: "\non your phone",
-                            style: GoogleFonts.roboto(
-                                color: Colors.black87, fontSize: 13.0))
-                      ]),
-                    )),
-                Padding(
-                    padding: const EdgeInsets.only(top: 50.0, right: 10.0),
-                    child: Image.asset(
-                      "assets/images/wel.png",
-                      fit: BoxFit.cover,
-                    )),
-                CustomButton(text: "Get started", color: secondColor, onPressed: (){
-                  Navigator.pushNamed(context, SignUpScreen.routeName);
-                })
-              ],
+                    padding: const EdgeInsets.only(top: 100.0, left: 25.0),
+                    child: AnimatedTextKit(
+                      repeatForever: true,
+                      pause: const Duration(seconds: 20),
+                      animatedTexts: [
+                        TyperAnimatedText('Enjoy',
+                            textStyle: GoogleFonts.audiowide(
+                                color: Colors.black87,
+                                fontSize: 35.0,
+                                fontWeight: FontWeight.w800,
+                                letterSpacing: 1.0),
+                          speed: const Duration(milliseconds: 350),
+                          curve: Curves.bounceIn
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: const EdgeInsets.only(top: 4.0, left: 25.0),
+                    // child: Text("cooking",
+                    //     style: GoogleFonts.audiowide(
+                    //         color: Colors.white,
+                    //         fontSize: 30.0,
+                    //         fontWeight: FontWeight.w400,
+                    //         letterSpacing: 1.0)),
+                    child: AnimatedTextKit(
+                      repeatForever: true,
+                      pause: const Duration(seconds: 13),
+                      animatedTexts: [
+                        TyperAnimatedText('cooking',
+                            textStyle: GoogleFonts.audiowide(
+                                color: thirdColor,
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 1.0),
+                            speed: const Duration(milliseconds: 370),
+                            curve: Curves.linear,
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.only(top: 20.0, left: 25.0),
+                      child: RichText(
+                        text: TextSpan(children: [
+                          TextSpan(
+                              text: "Delicious and detailed restaurant recipes",
+                              style: GoogleFonts.roboto(
+                                  color: Colors.black87, fontSize: 13.0)),
+                          TextSpan(
+                              text: "\non your phone",
+                              style: GoogleFonts.roboto(
+                                  color: Colors.black87, fontSize: 13.0))
+                        ]),
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.only(top: 50.0, right: 10.0),
+                      child: Image.asset(
+                        "assets/images/wel.png",
+                        fit: BoxFit.cover,
+                      )),
+                  CustomButton(text: "Get started", color: secondColor, onPressed: (){
+                    Navigator.pushNamed(context, SignUpScreen.routeName);
+                  })
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
